91d17bbbd98c73e052d6b6fa1d9c4d7e5d4a39e0
martamestroni <100764396+martamestroni@users.noreply.github.com>
2022-09-30 11:55:10 +0200

    No flux boundary conditions seem to work.

--- 2D_Diffusion_Circle.py
+++ 2D_Diffusion_Circle.py
@@ -5,20 +5,23 @@ num_steps = 20 # Number of time steps
 dt = (T-t)/num_steps # Time step size
 alpha = 3
 beta = 1.2
+R=1
+B_value =100
 
 # Diffusion coefficient
 D_=0.1
 
 
-import numpy
+import numpy as np
 import meshio
 import gmsh
 import sys
 import pygmsh
 from dolfinx import mesh, fem
 from dolfinx.io import XDMFFile, gmshio
-from dolfinx.fem import FunctionSpace, Function, Constant, locate_dofs_geometrical, dirichletbc, form, Expression
-import ufl
+from dolfinx.fem import FunctionSpace, Function, Constant, locate_dofs_geometrical,locate_dofs_topological, dirichletbc, form, Expression
+import dolfinx.mesh
+import ufl 
 from mpi4py import MPI
 from petsc4py import PETSc
 from petsc4py.PETSc import ScalarType
@@ -48,7 +51,7 @@ from petsc4py.PETSc import ScalarType
 # with XDMFFile(MPI.COMM_WORLD, "circle1.xdmf", "r") as xdmf:
 #        mesh = xdmf.read_mesh(name="Grid")
 
-# Try3
+# Try3 - KEEP TO WRITE FILE RO .xdmf FILE: good for inspecting the mesh
 # def create_mesh(mesh, cell_type, prune_z=False):
 #     cells = mesh.get_cells_type(cell_type)
 #     cell_data = mesh.get_cell_data("gmsh:physical", cell_type)
@@ -115,14 +118,38 @@ class exact_solution():
         return 1 + x[0]**2 + self.alpha * x[1]**2 + self.beta * self.t
 u_exact = exact_solution(alpha, beta, t)
 
-# BOUNDARY CONDITION
-u_D = fem.Function(V)
-u_D.interpolate(u_exact)
-tdim = domain.topology.dim
-fdim = tdim - 1
-domain.topology.create_connectivity(fdim, tdim)
-boundary_facets = mesh.exterior_facet_indices(domain.topology)
-bc = fem.dirichletbc(u_D, fem.locate_dofs_topological(V, fdim, boundary_facets))
+# BOUNDARY CONDITION for analytical solution
+# u_D = fem.Function(V)
+# u_D.interpolate(u_exact)
+# tdim = domain.topology.dim
+# fdim = tdim - 1
+# domain.topology.create_connectivity(fdim, tdim)
+# boundary_facets = mesh.exterior_facet_indices(domain.topology)
+# bc = fem.dirichletbc(u_D, fem.locate_dofs_topological(V, fdim, boundary_facets))
+
+# BOUNDARY condtion settng Neumann at the center and boarders
+x= ufl.SpatialCoordinate(domain)
+boundaries =[(1, lambda x: np.isclose(np.sqrt(x[0]**2+x[1]**2),R)),
+            (2, lambda x: np.isclose(np.sqrt(x[0]**2+x[1]**2),0))]
+
+facet_indices, facet_markers = [], []
+fdim = domain.topology.dim - 1
+for (marker, locator) in boundaries:
+    facets = dolfinx.mesh.locate_entities(domain, fdim, locator)
+    facet_indices.append(facets)
+    facet_markers.append(np.full_like(facets, marker))
+facet_indices = np.hstack(facet_indices).astype(np.int32)
+facet_markers = np.hstack(facet_markers).astype(np.int32)
+sorted_facets = np.argsort(facet_indices)
+facet_tag = dolfinx.mesh.meshtags(domain, fdim, facet_indices[sorted_facets], facet_markers[sorted_facets])
+
+domain.topology.create_connectivity(domain.topology.dim-1, domain.topology.dim)
+with XDMFFile(domain.comm, "facet_tags.xdmf", "w") as xdmf:
+    xdmf.write_mesh(domain)
+    xdmf.write_meshtags(facet_tag)
+
+ds = ufl.Measure("ds", domain=domain, subdomain_data=facet_tag)
+
 
 # DEFINIG VARIATIONAL FORMULATION
 u_n = fem.Function(V)
@@ -141,10 +168,50 @@ xdmf = XDMFFile(domain.comm, "2D_Diffusion_Circle.xdmf", "w")
 
 u, v = ufl.TrialFunction(V), ufl.TestFunction(V)
 F = u*v*ufl.dx + D*dt*ufl.dot(ufl.grad(u), ufl.grad(v))*ufl.dx - (u_n + dt*f)*v*ufl.dx
+
+# Finishing defining boundaries and apppending to F
+
+
+class BoundaryCondition():
+    def __init__(self, type, marker, values):
+        self._type = type
+        if type == "Dirichlet":
+            u_no = Function(V)
+            u_no.interpolate(values)
+            facets = facet_tag.find(marker)
+            dofs = locate_dofs_topological(V, fdim, facets)
+            self._bc = dirichletbc(u_D, dofs)
+        elif type == "Neumann":
+                self._bc = ufl.inner(values, v) * ds(marker)
+        elif type == "Robin":
+            self._bc = values[0] * ufl.inner(u-values[1], v)* ds(marker)
+        else:
+            raise TypeError("Unknown boundary condition: {0:s}".format(type))
+    @property
+    def bc(self):
+        return self._bc
+
+    @property
+    def type(self):
+        return self._type
+
+# Define the Dirichlet condition
+boundary_conditions = [BoundaryCondition("Neumann", 1, 0),
+                        BoundaryCondition("Neumann", 2, 0)
+]
+
+bcs = []
+for condition in boundary_conditions:
+    if condition.type == "Dirichlet":
+        bcs.append(condition.bc)
+    else:
+        F += condition.bc
+
+
 a = fem.form(ufl.lhs(F))
 L = fem.form(ufl.rhs(F))
 
-A = fem.petsc.assemble_matrix(a, bcs=[bc])
+A = fem.petsc.assemble_matrix(a)
 A.assemble()
 b = fem.petsc.create_vector(L)
 uh = fem.Function(V)
@@ -161,7 +228,7 @@ xdmf.write_function(u_n, t)
 for n in range(num_steps):
     # Update Diriclet boundary condition 
     u_exact.t+=dt
-    u_D.interpolate(u_exact)
+    # u_D.interpolate(u_exact)
     
     # Update the right hand side reusing the initial vector
     with b.localForm() as loc_b:
@@ -169,9 +236,9 @@ for n in range(num_steps):
     fem.petsc.assemble_vector(b, L)
     
     # Apply Dirichlet boundary condition to the vector
-    fem.petsc.apply_lifting(b, [a], [[bc]])
-    b.ghostUpdate(addv=PETSc.InsertMode.ADD_VALUES, mode=PETSc.ScatterMode.REVERSE)
-    fem.petsc.set_bc(b, [bc])
+    # fem.petsc.apply_lifting(b, [a], [[bcs]])
+    # b.ghostUpdate(addv=PETSc.InsertMode.ADD_VALUES, mode=PETSc.ScatterMode.REVERSE)
+    # fem.petsc.set_bc(b, [bcs])
 
     # Solve linear problem
     solver.solve(b, uh.vector)
@@ -186,15 +253,15 @@ for n in range(num_steps):
 
 xdmf.close()
 
-# Compute L2 error and error at nodes
-V_ex = fem.FunctionSpace(domain, ("CG", 2))
-u_ex = fem.Function(V_ex)
-u_ex.interpolate(u_exact)
-error_L2 = numpy.sqrt(domain.comm.allreduce(fem.assemble_scalar(fem.form((uh - u_ex)**2 * ufl.dx)), op=MPI.SUM))
-if domain.comm.rank == 0:
-    print(f"L2-error: {error_L2:.2e}")
+# # Compute L2 error and error at nodes
+# V_ex = fem.FunctionSpace(domain, ("CG", 2))
+# u_ex = fem.Function(V_ex)
+# u_ex.interpolate(u_exact)
+# error_L2 = np.sqrt(domain.comm.allreduce(fem.assemble_scalar(fem.form((uh - u_ex)**2 * ufl.dx)), op=MPI.SUM))
+# if domain.comm.rank == 0:
+#     print(f"L2-error: {error_L2:.2e}")
 
-# Compute values at mesh vertices
-error_max = domain.comm.allreduce(numpy.max(numpy.abs(uh.x.array-u_D.x.array)), op=MPI.MAX)
-if domain.comm.rank == 0:
-    print(f"Error_max: {error_max:.2e}")
+# # Compute values at mesh vertices
+# error_max = domain.comm.allreduce(np.max(np.abs(uh.x.array-u_D.x.array)), op=MPI.MAX)
+# if domain.comm.rank == 0:
+#     print(f"Error_max: {error_max:.2e}")
